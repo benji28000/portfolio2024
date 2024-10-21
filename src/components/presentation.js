76d33eafb649652fdbf005a633cfc71ed26b45d9
Benjamin Perez <95043947+benji28000@users.noreply.github.com>
2024-10-21 23:44:27 +0200

    Update presentation.js

--- src/components/presentation.js
+++ src/components/presentation.js
@@ -21,6 +21,7 @@ const Presentation = () => {
                             <h2 className="text-2xl font-bold mb-4">Développeur web fullstack</h2>
                             <p>
                                 Je suis actuellement étudiant en L3 MIAGE à l'Université d'Orléans.
+                                <br>
                                 Je compte poursuivre mes études jusqu'en master MIAGE et devenir développeur fullstack puis chef de projet.
                             </p>
                             <div className="flex mt-5 space-x-10">
